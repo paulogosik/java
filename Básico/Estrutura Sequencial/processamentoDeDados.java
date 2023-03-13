859a3b8391a0d43fd46ca3f2d8f946149695d912
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-13 16:15:24 -0300

    Create processamentoDeDados.java

---
+++ Básico/Estrutura Sequencial/processamentoDeDados.java
@@ -0,0 +1,28 @@
+public class processamentoDeDados {
+    public static void main(String[] args) {
+        
+        // Situação #1-----------------------------
+        int x;
+        double y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println("=====================");
+
+        // Situação #2-------------------------------
+        double b, B, h, area;
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+        area = (b + B) / 2 * h;
+
+        System.out.println(area);
+        System.out.println("=====================");
+
+        //Situação #3--------------------------------
+
+    }
+}
