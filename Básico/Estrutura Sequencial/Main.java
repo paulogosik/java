a85358578565f10c4524f79090d38cc7438feff4
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-13 10:30:23 -0300

    Update Main

--- Básico/Estrutura Sequencial/Main.java
+++ Básico/Estrutura Sequencial/Main.java
@@ -1,6 +1,6 @@
-class HelloWorld {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("");
+        System.out.println("Olá, Mundo!");
     }
 }
