9b22cb3b9974865aaf62acca29d5bd353dece038
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-13 14:21:58 -0300

    Update ExpAritimeticas.java

--- Básico/Estrutura Sequencial/ExpAritimeticas.java
+++ Básico/Estrutura Sequencial/ExpAritimeticas.java
@@ -1,17 +1,18 @@
 public class ExpAritimeticas {
     public static void main(String[] args) {
-        System.out.println("--------------------------");
-        System.out.println("Expressões Aritiméticas: ");
-        System.out.println("(+) - Adição ");
-        System.out.println("(-) - Subtração");
-        System.out.println("(*) - Multiplicação");
-        System.out.println("(/) - Divisão");
-        System.out.println("(%) - Mod");
-        System.out.println("--------------------------");
+      System.out.println("--------------------------");
+      System.out.println("Expressões Aritiméticas: ");
+      System.out.println("(+) - Adição ");
+      System.out.println("(-) - Subtração");
+      System.out.println("(*) - Multiplicação");
+      System.out.println("(/) - Divisão");
+      System.out.println("(%) - Mod");
+      System.out.println("--------------------------");
     }
 }
 
-/*(+) - Adição 
+/*
+  (+) - Adição 
   (-) - Subtração
   (*) - Multiplicação
   (/) - Divisão
