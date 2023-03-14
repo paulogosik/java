f8ac61f1e4fcede3cd8784556b8ba07a441d991f
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-14 10:24:42 -0300

    Update calculoSalario.java

--- Estrutura Sequencial/Exercícios/calculoSalario.java
+++ Estrutura Sequencial/Exercícios/calculoSalario.java
@@ -1,7 +1,31 @@
 package Exercícios;
 
+import java.util.Scanner;
+import java.util.Locale;
 public class calculoSalario {
     public static void main(String[] args) {
-        
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        String number;
+        int hours;
+        double value, salary;
+
+        System.out.printf("=> Qual é o seu número? ");
+        number = input.nextLine();
+        System.out.printf("=> Qual foi a quantidade de horas trabalhadas? ");
+        hours = input.nextInt();
+        System.out.printf("=> Qual o valor que você recebe por hora? ");
+        value = input.nextDouble();
+        System.out.println("-----------------------------");
+
+        salary = value * hours;
+
+        System.out.printf("=> Número: %s\n", number);
+        System.out.printf("=> Salary: U$%.2f\n", salary);
+
+        input.close();
+
     }
 }
