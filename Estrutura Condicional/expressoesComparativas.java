7460cd0d8e55773a536c38e6f696b55a443c8d03
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-14 14:17:19 -0300

    Update expressoesComparativas.java

--- Estrutura Condicional/expressoesComparativas.java
+++ Estrutura Condicional/expressoesComparativas.java
@@ -1,7 +1,22 @@
 public class expressoesComparativas {
     public static void main(String[] args) {
 
-        
+        /*
+         * (>) - Maior
+         * (<) - Menor
+         * (>=) - Maior ou igual
+         * (<=) - Menor ou igual
+         * (==) - Igual
+         * (!=) - Diferente
+         */
+
+        int x = 5;
+        if (x > 6) {
+            System.out.println("Verdadeiro!");
+        }
+        else {
+            System.out.println("Falso!");
+        }
 
     }
 }
