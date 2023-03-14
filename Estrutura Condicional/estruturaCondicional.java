1c423d366cfc963ff6a91e5d609f6b2b1307398d
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-14 15:23:44 -0300

    Create estruturaCondicional.java

---
+++ Estrutura Condicional/estruturaCondicional.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class estruturaCondicional {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int hours;
+
+        System.out.printf("=> Que horas são? (Formato 24Hs, Ex.: 18, 22) ");
+        hours = input.nextInt();
+        
+        if (hours <= 12 && hours >= 6) {
+            System.out.println("Bom dia!");
+        }
+        else if (hours <= 18 && hours > 12) {
+            System.out.println("Boa tarde!");
+        }
+        else {
+            System.out.println("Boa noite!");
+        }
+
+        input.close();
+
+    }
+}
