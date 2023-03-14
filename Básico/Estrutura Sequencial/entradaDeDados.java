19ce6d6281e0681837033a1330c90f316ca335e1
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-14 08:44:05 -0300

    Update entradaDeDados.java

--- Básico/Estrutura Sequencial/entradaDeDados.java
+++ Básico/Estrutura Sequencial/entradaDeDados.java
@@ -4,14 +4,18 @@ public class entradaDeDados {
 
         String nome;
         int idade;
+        double salario;
 
         Scanner input = new Scanner(System.in);
         System.out.println("=> Qual é o seu nome? ");
         nome = input.nextLine();
-        System.out.println("=> Quantos anos você tem?");
+        System.out.println("=> Quantos anos você tem? ");
         idade = input.nextInt();
+        System.out.println("=> Qual o seu salário? ");
+        salario = input.nextDouble();
 
-        System.out.printf("=> Seu nome é %s e você tem %d anos!", nome, idade);
+
+        System.out.printf("=> Seu nome é %s, você tem %d anos e recebe R$%.2f por mês!", nome, idade, salario);
         input.close();
 
     }
