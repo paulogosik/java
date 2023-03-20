3a85b5da3f2bce65bd8032c6e21007697550cf61
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-20 17:29:22 -0300

    Create SwitchCase.java

---
+++ Estrutura Condicional/SwitchCase.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String day;
+
+        System.out.printf("=> Informe um número de 1 a 7: ");
+        int x = input.nextInt();
+
+        switch(x) {
+            case 1:
+                day = "Domingo";
+                break;
+            case 2:
+                day = "Segunda-feira";
+                break;
+            case 3:
+                day = "Terça-feira";
+                break;
+            case 4:
+                day = "Quarta-feira";
+                break;
+            case 5:
+                day = "Quinta-feira";
+                break;
+            case 6:
+                day = "Sexta-feira";
+                break;
+            case 7:
+                day = "Sábado";
+                break;
+            default:
+                day = "Valor inválido";       
+        }
+        
+        System.out.printf("=> Dia da semana: %s", day);
+
+        input.close();
+
+    }
+}
