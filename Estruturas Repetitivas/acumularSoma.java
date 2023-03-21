60cd2ab2bed639dd3ac919a19bcd614c0f445102
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-21 09:08:45 -0300

    Create acumularSoma.java

---
+++ Estruturas Repetitivas/acumularSoma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class acumularSoma {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int x;
+        int soma = 0;
+
+        System.out.printf("=> Digite um número qualquer: ");
+        x = input.nextInt();
+
+        while (x != 0) {
+            soma += x;
+            System.out.printf("=> Digite um número qualquer: ");
+            x = input.nextInt();
+        }
+
+        System.out.printf("=> Soma: %d", soma);
+        input.close();
+
+
+    }
+}
