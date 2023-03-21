f0afed0b199863f49ad69d97df17e3a64d012bce
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-21 10:37:21 -0300

    Update acumularSomaWhile.java

--- Estruturas Repetitivas/While/acumularSomaWhile.java
+++ Estruturas Repetitivas/While/acumularSomaWhile.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class acumularSoma {
+public class acumularSomaWhile {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
