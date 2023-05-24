791695a75e78d24ed1a8cf5a9c541d4a36fdb36f
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-05-24 14:12:50 -0300

    Update Main.java

---
+++ Programação Orientada a Objetos/Introdução/TesteAreaTriangulos/Main.java
@@ -0,0 +1,16 @@
+package TesteAreaTriangulos;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double xA, xB, xC, yA, yB, yC;
+
+        input.close();
+    }
+    
+}
