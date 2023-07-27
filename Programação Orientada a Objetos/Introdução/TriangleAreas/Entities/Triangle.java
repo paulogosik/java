de4f20eb684704ccf54a05bcbb7d1d83b17bf56a
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-07-27 10:08:51 -0300

    Update Triangle.java

--- Programação Orientada a Objetos/Introdução/TriangleAreas/Entities/Triangle.java
+++ Programação Orientada a Objetos/Introdução/TriangleAreas/Entities/Triangle.java
@@ -1,6 +1,8 @@
 package TriangleAreas.Entities;
 
 public class Triangle {
+    // Foram criados membros para a classe Triange{}
+    // Sendo eles a, b, c; Todos double e public
     public double a;
     public double b;
     public double c;
