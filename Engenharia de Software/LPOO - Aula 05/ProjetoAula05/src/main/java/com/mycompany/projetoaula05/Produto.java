04e38779dc791ab5a5cda7be945ec69f6c18c691
paulogosik <paulogosikmoita@gmail.com>
2024-03-25 20:09:02 -0300

    Updating Aula05 project

---
+++ Engenharia de Software/LPOO - Aula 05/ProjetoAula05/src/main/java/com/mycompany/projetoaula05/Produto.java
@@ -0,0 +1,24 @@
+package com.mycompany.projetoaula05;
+
+public class Produto {
+    String nome;
+    double preco;
+    int qtEstoque;
+    
+    public void aumentarPreco(double percAumento){
+        double aumento = (preco * percAumento) / 100;
+        preco += aumento;
+    }
+    
+    public void inserirEstoque(int qtInserir){
+        qtEstoque += qtInserir;
+    }
+    
+    public boolean retirarEstoque(int qtRetirar){
+        if (qtEstoque >= qtRetirar){
+            qtEstoque -= qtRetirar;
+            return true;
+        }
+        return false;
+    }
+}
