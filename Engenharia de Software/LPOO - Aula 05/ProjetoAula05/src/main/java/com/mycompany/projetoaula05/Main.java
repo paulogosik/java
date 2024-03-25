42449f4d085a721247b87cfa4c28060320532158
paulogosik <paulogosikmoita@gmail.com>
2024-03-25 19:17:31 -0300

    Creating Aula05 project

---
+++ Engenharia de Software/LPOO - Aula 05/ProjetoAula05/src/main/java/com/mycompany/projetoaula05/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoaula05;
+
+/**
+ *
+ * @author pgosi
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
